767e89a42ebe4a2d873277b6b183c68d714aafc2
Евгений <>
2023-03-02 22:52:20 +0300

    initial commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Евгений on 13.12.22.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "358389cc2e7b7f987ac85f1075b911c6"
